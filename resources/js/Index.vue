65ea0039aeeeaf0eb11fb0b45102860a6671f20b
Elvan Hidayat <elvanhd@gmail.com>
2023-03-22 09:58:04 +0700

    change navbar colors

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        <nav class="navbar bg-white border-bottom navbar-light">
+        <nav class="navbar navbar-dark bg-primary border-bottom">
             <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }"
                 >HomestayBooking</router-link
             >
-            <router-link class="btn nav-button" :to="{ name: 'home' }">Second</router-link>
+            <router-link class="btn nav-button text-white" :to="{ name: 'home' }"
+                >Second</router-link
+            >
         </nav>
 
         <div class="container mt-4 mb-4 pr-4 pl-4">
