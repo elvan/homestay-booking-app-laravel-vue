8e9b69953b0ef7420b40ff70118082080b24e263
Elvan Hidayat <elvanhd@gmail.com>
2023-03-22 09:30:36 +0700

    create bookable details route and page

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
         <nav class="navbar bg-white border-bottom navbar-light">
-            <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }">HomestayBooking</router-link>
-            <router-link class="btn nav-button" :to="{ name: 'second' }">Second</router-link>
+            <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }"
+                >HomestayBooking</router-link
+            >
+            <router-link class="btn nav-button" :to="{ name: 'home' }">Second</router-link>
         </nav>
 
         <div class="container mt-4 mb-4 pr-4 pl-4">
