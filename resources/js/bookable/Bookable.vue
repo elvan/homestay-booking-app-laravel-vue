f3f44f8667d86ca253d682bb0250b3870e629987
Elvan Hidayat <elvanhd@gmail.com>
2023-03-22 09:46:23 +0700

    display bookable details inside css grid

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>xxx</div>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <div v-if="!loading">
+                        <h2>{{ bookable.title }}</h2>
+                        <hr />
+                        <article>{{ bookable.description }}</article>
+                    </div>
+                    <div v-else>Loading...</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">availability & prices</div>
+    </div>
 </template>
 
 <script>
@@ -7,11 +21,14 @@ export default {
     data() {
         return {
             bookable: null,
+            loading: false,
         };
     },
     created() {
+        this.loading = true;
         axios.get(`/api/bookables/${this.$route.params.id}`).then((response) => {
             this.bookable = response.data;
+            this.loading = false;
         });
     },
 };
