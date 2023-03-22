611c8251cb23b96fda9b6cc4d0d96c5476fcb6a1
Elvan Hidayat <elvanhd@gmail.com>
2023-03-22 09:21:49 +0700

    bootstrap flexbox utility classes

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -3,7 +3,11 @@
         <div v-if="loading">Data is loading...</div>
         <div v-else>
             <div class="row mb-4" v-for="row in rows" :key="'row' + row">
-                <div class="col" v-for="(bookable, column) in bookablesInRow(row)" :key="'row' + row + column">
+                <div
+                    class="col d-flex align-items-stretch"
+                    v-for="(bookable, column) in bookablesInRow(row)"
+                    :key="'row' + row + column"
+                >
                     <bookable-list-item
                         :item-title="bookable.title"
                         :item-description="bookable.description"
