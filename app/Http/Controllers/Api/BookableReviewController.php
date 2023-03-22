32ae4b748d27404e881918f01d2ec483f0cfc5c1
Elvan Hidayat <elvanhd@gmail.com>
2023-03-22 17:52:14 +0700

    serialize review model resource

--- app/Http/Controllers/Api/BookableReviewController.php
+++ app/Http/Controllers/Api/BookableReviewController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use App\Bookable;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BookableReviewIndexResource;
 
 class BookableReviewController extends Controller
 {
+
     public function __invoke($id)
     {
         $bookable = Bookable::findOrFail($id);
 
-        return $bookable->reviews()->latest()->get();
+        return BookableReviewIndexResource::collection(
+            $bookable->reviews()->latest()->get()
+        );
     }
 }
