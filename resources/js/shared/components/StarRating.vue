7c1a13bf5715ddf00ad9a1eeee11291450fbbd75
Elvan Hidayat <elvanhd@gmail.com>
2023-03-23 07:22:17 +0700

    two-way data binding using v-model

--- resources/js/shared/components/StarRating.vue
+++ resources/js/shared/components/StarRating.vue
@@ -4,14 +4,14 @@
             class="fas fa-star"
             v-for="star in fullStars"
             :key="'full' + star"
-            @click="$emit('rating:changed', star)"
+            @click="$emit('input', star)"
         ></i>
         <i class="fas fa-star-half-alt" v-if="halfStar"></i>
         <i
             class="far fa-star"
             v-for="star in emptyStars"
             :key="'empty' + star"
-            @click="$emit('rating:changed', fullStars + star)"
+            @click="$emit('input', fullStars + star)"
         ></i>
     </div>
 </template>
@@ -19,18 +19,18 @@
 <script>
 export default {
     props: {
-        rating: Number,
+        value: Number,
     },
     computed: {
         halfStar() {
-            const fraction = Math.round((this.rating - Math.floor(this.rating)) * 100);
+            const fraction = Math.round((this.value - Math.floor(this.value)) * 100);
             return fraction > 0 && fraction < 50;
         },
         fullStars() {
-            return Math.round(this.rating);
+            return Math.round(this.value);
         },
         emptyStars() {
-            return 5 - Math.ceil(this.rating);
+            return 5 - Math.ceil(this.value);
         },
     },
 };
