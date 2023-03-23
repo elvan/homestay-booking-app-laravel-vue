429af76899bcbe4e99891c0a64425d7940476b89
Elvan Hidayat <elvanhd@gmail.com>
2023-03-23 16:46:20 +0700

    fix navigation link and props type

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -4,9 +4,6 @@
             <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }"
                 >HomestayBooking</router-link
             >
-            <router-link class="btn nav-button text-white" :to="{ name: 'home' }"
-                >Second</router-link
-            >
         </nav>
 
         <div class="container mt-4 mb-4 pr-4 pl-4">
