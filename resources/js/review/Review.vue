c8066b32ca39f9447c8b866041227cb633944a99
Elvan Hidayat <elvanhd@gmail.com>
2023-03-23 07:10:44 +0700

    create custom event in the star-rating component

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -2,13 +2,30 @@
     <div>
         <div class="form-group">
             <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
-            <star-rating :rating="5" class="fa-3x"></star-rating>
+            <star-rating
+                :rating="5"
+                class="fa-3x"
+                v-on:rating:changed="review.rating = $event"
+            ></star-rating>
         </div>
         <div class="form-group">
-            <label for="content" class="text-muted">Describe your experience with</label>
+            <label for="content" class="text-muted">Describe your expirience with</label>
             <textarea name="content" cols="30" rows="10" class="form-control"></textarea>
         </div>
 
         <button class="btn btn-lg btn-primary btn-block">Submit</button>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            review: {
+                rating: 5,
+                content: null,
+            },
+        };
+    },
+};
+</script>
