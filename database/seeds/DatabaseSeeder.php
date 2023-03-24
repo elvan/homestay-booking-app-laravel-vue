6abce5af3f01d427016951c9e90b5fb6c4f10bbe
Elvan Hidayat <elvanhd@gmail.com>
2023-03-24 19:04:32 +0700

    fix booking factory and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -26,7 +26,8 @@ class DatabaseSeeder extends Seeder
 
                 $booking = Booking::make([
                     'from' => $from,
-                    'to' => $to
+                    'to' => $to,
+                    'price' => random_int(200, 5000)
                 ]);
                 $bookings->push($booking);
             }
